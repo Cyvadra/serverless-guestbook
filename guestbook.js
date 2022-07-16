2299fb5173932ab223aa2230d66cd1334f8a1656
Jason89757 <dj@lifesdk.com>
2022-07-16 14:03:05 +0800

    Update apiUrl
    Fill blank with IBM Cloud API Url.

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://e5e97e5d.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
